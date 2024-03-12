616e6810f1ea6625f378bb17628ae667ff7173ec
Narayani <125910649+narayani-gf@users.noreply.github.com>
2024-03-11 23:59:48 -0600

    Ejercicio 1 práctica app con hilos C#

---
+++ AppConHilos/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace AppConHilos;
+
+class Program
+{
+    public static void DoTrabajoPesado()
+    {
+        Console.WriteLine("DoTrabajoPesado: Estoy levantando un camión!!");
+        Thread.Sleep(1000);
+        Console.WriteLine("DoTrabajoPesado: Cansado! Necesito una siesta de 3seg.");
+        Thread.Sleep(1000);
+        Console.WriteLine("DoTrabajoPesado: 1...");
+        Thread.Sleep(1000);
+        Console.WriteLine("DoTrabajoPesado: 2...");
+        Thread.Sleep(1000);
+        Console.WriteLine("DoTrabajoPesado: 3...");
+        Console.WriteLine("DoTrabajoPesado: Ya desperté");
+    }
+
+    public static void DoAlgo()
+    {
+        Console.WriteLine("DoAlgo: Oye! Haciendo algo aquí!");
+        for (int i = 0; i < 20; i++)
+            Console.Write($"{i} ");
+        Console.WriteLine();
+        Console.WriteLine("DoAlgo: Ya terminé.");
+    }
+
+    //Este es el punto de entrada de un programa C#
+    static void Main(string[] args)
+    {
+        //La ejecución del programa inicia aquí
+        Console.WriteLine("El Main thread comienza aquí.");
+
+        //Este método tarda 4 segundos en terminar.
+        //Crea un hilo y llama a un método en segundo plano.
+        Thread backgroundThread = new Thread(new ThreadStart(Program.DoTrabajoPesado));
+        //Inicia el hilo
+        backgroundThread.Start();
+
+        //Este método no se tarda nada.
+        Program.DoAlgo();
+
+        //La ejecución termina aquí.
+        Console.WriteLine("El Main thread termina aquí.");
+    }
+}
